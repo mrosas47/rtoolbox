b15c996a4770104211219e612e628b11a1c2304f
mrosas47 <mrosas1690@gmail.com>
2022-11-02 08:43:09 -0300

    added function pass()

---
+++ R/pass.R
@@ -0,0 +1,12 @@
+#' Pass: do nothing
+#'
+#' @return Nothing!
+#' @export pass
+#'
+#' @examples No example is necessary. Python's `pass` statement.
+
+pass <- function () {
+  
+  # Nothing! :)
+  
+}
