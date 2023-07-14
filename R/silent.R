c0884aad1afd6bc81fcc241ff41c6262db63fc45
mrosas47 <mrosas1690@gmail.com>
2023-07-14 14:42:04 -0400

    fixed bug

--- R/silent.R
+++ R/silent.R
@@ -1,3 +1,11 @@
+#' Silent -- no messages or warnings
+#'
+#' @param expr 
+#'
+#' @return expr with no messages or warnings
+#' @export silent
+#'
+#' @examples # none
 silent <- function(expr) {
   
   suppressWarnings({
