5a4876b3352ae5ca5a04680564cd0f4bf6722df3
Martín Rosas Araya <mrosas1690@gmail.com>
2023-11-29 10:42:26 -0300

    added operator

--- R/assignment_pipe.R
+++ R/assignment_pipe.R
@@ -1,5 +1,6 @@
-#' @title  Magrittr's assignment pipe
+#' @title  `magrittr`'s assignment pipe
 #' @description Literally the same as magrittr's assignment pipe `%<>%`. Just did not want to call `library(magrittr, include.only = '%<>%')` every time I wanted to use it, and since I do load `rtoolbox` every time it was simpler to have it here.
+#' @import magrittr
 #' @export %<>%
 #' @examples # Will do this later
 #' @return Same logic as magrittr's assignment pipe
---
+++ R/plus_equal.R
@@ -0,0 +1,8 @@
+#' @title  `roperators`'s plus-equal
+#' @description Literally the same as roperators's `%+=%`. Just did not want to call `library(roperators, include.only = '%+=%')` every time I wanted to use it, and since I do load `rtoolbox` every time it was simpler to have it here.
+#' @import roperators
+#' @export %+=%
+#' @examples # Will do this later
+#' @return Same logic as magrittr's assignment pipe
+
+`%+=%` <- roperators::`%+=%`
